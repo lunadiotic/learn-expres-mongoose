e5ee60321e53aa3439eaa658fa0f838b2a5335b5
aimeliala <idstack@outlook.co.id>
2023-04-03 13:11:27 +0700

    menghapus data di database

--- index.js
+++ index.js
@@ -57,6 +57,12 @@ app.put('/products/:id', async (req, res) => {
     res.redirect(`/products/${product._id}`)
 })
 
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params
+    await Product.findByIdAndDelete(id)
+    res.redirect('/products')
+})
+
 
 app.listen(3000, () => {
     console.log('shop app listening on http://127.0.0.1:3000')

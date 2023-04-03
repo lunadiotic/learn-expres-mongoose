959a59876fd89696d17274ce4bc2ec618176c061
aimeliala <idstack@outlook.co.id>
2023-04-03 13:19:28 +0700

    membuat filter kategori produk

--- index.js
+++ index.js
@@ -25,8 +25,14 @@ app.get('/', (req, res) => {
 })
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', { products })
+    const { category } = req.query
+    if (category) {
+        const products = await Product.find({ category })
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
 })
 
 app.get('/products/create', (req, res) => {

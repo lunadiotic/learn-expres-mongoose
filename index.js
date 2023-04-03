0672213a9420ac67145f4b363244ac9c8989bebc
aimeliala <idstack@outlook.co.id>
2023-04-03 12:58:17 +0700

    mengubah data di database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express')
+const methodOverride = require('method-override')
 const mongoose = require('mongoose')
 const app = express()
 
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://127.0.0.1/shop_db')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
@@ -47,6 +49,12 @@ app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params
     const product = await Product.findById(id)
     res.render('products/edit', { product })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true })
+    res.redirect(`/products/${product._id}`)
 })
 
 

c4640e6f5ed763b1dafda5efd47e4568a0116021
aimeliala <idstack@outlook.co.id>
2023-04-03 12:16:37 +0700

    menyimpan data ke dalam database

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ mongoose.connect('mongodb://127.0.0.1/shop_db')
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
     res.send('Hello World')
@@ -28,6 +29,12 @@ app.get('/products', async (req, res) => {
 
 app.get('/products/create', (req, res) => {
     res.render('products/create')
+})
+
+app.post('/products', async (req, res) => {
+    const product = new Product(req.body)
+    await product.save()
+    res.redirect(`/products/${product._id}`)
 })
 
 app.get('/products/:id', async (req, res) => {
